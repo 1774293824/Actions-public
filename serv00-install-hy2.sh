6c3051a3d086f4a34a66188ffa4d9567cd8a915b
1774293824 <95674405+1774293824@users.noreply.github.com>
2024-09-16 12:04:06 +0800

    Update serv00-install-hy2.sh

--- serv00-install-hy2.sh
+++ serv00-install-hy2.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export UUID=${UUID:-'a03e977f-6491-42a2-b56d-abbab6c3a9ac'} 
 
 # 设置脚本运行时的工作目录
 WORK_DIR="./sing-box"
@@ -14,6 +15,25 @@ wget -O 's-box.zip' 'https://raw.githubusercontent.com/1774293824/Actions-public
 echo "解压 s-box.zip..."
 unzip s-box.zip || exit 1
 
+# 获取用户输入
+read -p "请输入 IP 地址  : " IP
+read -p "请输入hysteria2端口号 : " PORT1
+read -p "请输入vless端口号 : " PORT2
+read -p "请输入tuic端口号 : " PORT3
+
+# 替换配置文件中的占位符
+CONFIG_FILE="config.json"
+if [ -f "$CONFIG_FILE" ]; then
+    echo "替换配置文件中的占位符..."
+    sed -i "s/{{IP}}/$IP/g" "$CONFIG_FILE"
+    sed -i "s/33333/$PORT1/g" "$CONFIG_FILE"
+    sed -i "s/44444/$PORT2/g" "$CONFIG_FILE"
+    sed -i "s/55555/$PORT3/g" "$CONFIG_FILE"
+else
+    echo "配置文件 $CONFIG_FILE 不存在！"
+    exit 1
+fi
+
 # 设置文件权限
 echo "设置文件权限 'web'..."
 chmod +x web || exit 1
@@ -24,4 +44,4 @@ screen -dmS box /home/yhwadaowajd/sing-box/web run || exit 1
 
 echo "服务已成功启动。要重新附加到屏幕会话，请使用：screen -r box"
 echo "请测试下面的订阅:"
-echo "hysteria2://a03e977f-6491-42a2-b56d-abbab6c3a9ac@panel11.serv00.com:63033/?sni=www.bing.com&alpn=h3&insecure=1#VeryGood_v11"
+echo "hysteria2://$UUID@panel11.serv00.com:$PORT1/?sni=www.bing.com&alpn=h3&insecure=1#VeryGood_v11"
