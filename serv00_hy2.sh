c3374e36c83a486ebfed4ed7633f8f800357537d
1774293824 <95674405+1774293824@users.noreply.github.com>
2024-09-16 14:23:52 +0800

    Update serv00_hy2.sh

--- serv00_hy2.sh
+++ serv00_hy2.sh
@@ -1,42 +1,33 @@
 #!/bin/sh
+set -e
 export UUID=${UUID:-'a03e977f-6491-42a2-b56d-abbab6c3a9ac'} 
 USERNAME=$(whoami)
 
 WORK_DIR="./sing-box"
+mkdir -p "$WORK_DIR" && cd "$WORK_DIR"
 
-mkdir -p "$WORK_DIR" && cd "$WORK_DIR" || exit 1
-
-# 下载压缩文件
+# 下载并解压缩文件
 echo "下载 s-box.zip..."
-wget -O 's-box.zip' 'https://raw.githubusercontent.com/1774293824/Actions-public/main/s-box.zip' || exit 1
+wget -O 's-box.zip' 'https://raw.githubusercontent.com/1774293824/Actions-public/main/s-box.zip' && unzip s-box.zip
 
-echo "解压 s-box.zip..."
-unzip s-box.zip || exit 1
+# 获取用户输入的函数
+prompt_for_input() {
+    local prompt_msg=$1
+    local default_value=$2
+    read -p "$(echo -e '\033[0;32m'"$prompt_msg"' (默认 '"$default_value"'): \033[0m')" input
+    echo "${input:-$default_value}"
+}
 
-# 获取用户输入
-DEFAULT_IP='31.186.87.205'
-read -p "$(echo -e '\033[0;32m请输入 IP地址(默认 '"$DEFAULT_IP"'): \033[0m')" IP
-IP=${IP:-$DEFAULT_IP}
-
-# 设置默认端口号
-DEFAULT_PORT1=21382
-read -p "$(echo -e '\033[0;32m请输入 hysteria2 端口号 (默认 '"$DEFAULT_PORT1"'): \033[0m')" PORT1
-PORT1=${PORT1:-$DEFAULT_PORT1}
-DEFAULT_PORT2=23517
-read -p "$(echo -e '\033[0;32m请输入 vless 端口号 (默认 '"$DEFAULT_PORT2"'): \033[0m')" PORT2
-PORT2=${PORT2:-$DEFAULT_PORT2}
-DEFAULT_PORT3=12225
-read -p "$(echo -e '\033[0;32m请输入 tuic 端口号 (默认 '"$DEFAULT_PORT3"'): \033[0m')" PORT3
-PORT3=${PORT3:-$DEFAULT_PORT3}
+IP=$(prompt_for_input "请输入 IP地址" '31.186.87.205')
+PORT1=$(prompt_for_input "请输入 hysteria2 端口号" '21382')
+PORT2=$(prompt_for_input "请输入 vless 端口号" '23517')
+PORT3=$(prompt_for_input "请输入 tuic 端口号" '12225')
 
 # 替换配置文件中的占位符
 CONFIG_FILE="config.json"
 if [ -f "$CONFIG_FILE" ]; then
     echo "替换配置文件中的占位符..."
-    sed -i '' "s/{{IP}}/$IP/g" "$CONFIG_FILE"
-    sed -i '' "s/33333/$PORT1/g" "$CONFIG_FILE"
-    sed -i '' "s/44444/$PORT2/g" "$CONFIG_FILE"
-    sed -i '' "s/55555/$PORT3/g" "$CONFIG_FILE"
+    sed -i "s/{{IP}}/$IP/g; s/33333/$PORT1/g; s/44444/$PORT2/g; s/55555/$PORT3/g" "$CONFIG_FILE"
 else
     echo "配置文件 $CONFIG_FILE 不存在！"
     exit 1
@@ -44,11 +35,11 @@ fi
 
 # 设置文件权限
 echo "设置文件权限 'web'..."
-chmod +x web || exit 1
+chmod 755 web
 
 # 启动服务
 echo "启动服务..."
-screen -dmS box /home/${USERNAME}/sing-box/web run || exit 1
+screen -dmS box /home/${USERNAME}/sing-box/web run
 
 echo "服务已成功启动。要重新附加到屏幕会话，请使用：screen -r box"
 echo "请测试下面的订阅:"
