91df6bb12d4aa73de761fb0ec9d318a648fcf5a8
1774293824 <95674405+1774293824@users.noreply.github.com>
2024-09-16 12:37:45 +0800

    Update serv00-install-hy2.sh

--- serv00-install-hy2.sh
+++ serv00-install-hy2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export UUID=${UUID:-'a03e977f-6491-42a2-b56d-abbab6c3a9ac'} 
-
+USERNAME=$(whoami)
 # 设置脚本运行时的工作目录
 WORK_DIR="./sing-box"
 
@@ -22,9 +22,17 @@ echo "当前目录文件有：$(ls)"
 
 # 获取用户输入
 read -p "请输入 IP 地址  : " IP
-read -p "请输入hysteria2端口号 : " PORT1
-read -p "请输入vless端口号 : " PORT2
-read -p "请输入tuic端口号 : " PORT3
+
+# 设置默认端口号
+DEFAULT_PORT1=21382
+read -p "请输入hysteria2端口号 (默认 $DEFAULT_PORT1): " PORT1
+PORT1=${PORT1:-$DEFAULT_PORT1}
+DEFAULT_PORT2=23517
+read -p "请输入vless端口号 (默认 $DEFAULT_PORT2): " PORT2
+PORT2=${PORT2:-$DEFAULT_PORT2}
+DEFAULT_PORT3=12225
+read -p "请输入tuic端口号 (默认 $DEFAULT_PORT3): " PORT3
+PORT3=${PORT3:-$DEFAULT_PORT3}
 
 # 替换配置文件中的占位符
 CONFIG_FILE="config.json"
@@ -45,8 +53,8 @@ chmod +x web || exit 1
 
 # 启动服务
 echo "启动服务..."
-screen -dmS box /home/yhwadaowajd/sing-box/web run || exit 1
+screen -dmS box /home/${USERNAME}/sing-box/web run || exit 1
 
 echo "服务已成功启动。要重新附加到屏幕会话，请使用：screen -r box"
 echo "请测试下面的订阅:"
-echo "hysteria2://$UUID@panel11.serv00.com:$PORT1/?sni=www.bing.com&alpn=h3&insecure=1#VeryGood_v11"
+echo "hysteria2://$UUID@$IP:$PORT1/?sni=www.bing.com&alpn=h3&insecure=1#VeryGood_v11"
