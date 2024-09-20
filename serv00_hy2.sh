de0320b779259ac73a165d4cb651b63d6d7745d1
1774293824 <95674405+1774293824@users.noreply.github.com>
2024-09-20 16:35:27 +0800

    Update serv00_hy2.sh

--- serv00_hy2.sh
+++ serv00_hy2.sh
@@ -18,16 +18,15 @@ prompt_for_input() {
     echo "${input:-$default_value}"
 }
 
-IP=$(prompt_for_input "请输入 IP地址" '31.186.87.205')
+IP=$(prompt_for_input "请输入 IP地址" '128.204.223.118')
 PORT1=$(prompt_for_input "请输入 hysteria2的udp端口号" '21382')
-PORT2=$(prompt_for_input "请输入 vless的tcp端口号" '23517')
-PORT3=$(prompt_for_input "请输入 tuic的udp端口号" '12225')
+PORT2=$(prompt_for_input "请输入 tuic的udp端口号" '23517')
 
 # 替换配置文件中的占位符
 CONFIG_FILE="config.json"
 if [ -f "$CONFIG_FILE" ]; then
     echo "替换配置文件中的占位符..."
-    sed -i '' "s/{{IP}}/$IP/g; s/33333/$PORT1/g; s/44444/$PORT2/g; s/55555/$PORT3/g" "$CONFIG_FILE"
+    sed -i '' "s/{{IP}}/$IP/g; s/33333/$PORT1/g; s/44444/$PORT2/g" "$CONFIG_FILE"
 else
     echo "配置文件 $CONFIG_FILE 不存在！"
     exit 1
@@ -43,5 +42,5 @@ echo "启动服务: successful\n服务已成功启动。要重新附加到屏幕
 echo ""
 echo -e "\033[0;32m hysteria2://$UUID@$IP:$PORT1/?sni=www.bing.com&alpn=h3&insecure=1#${USERNAME} \033[0m"
 echo ""
-echo -e "\033[0;32m tuic://$UUID:password123@$IP:$PORT3?sni=www.bing.com&alpn=h3&congestion_control=bbr#${USERNAME} \033[0m"
+echo -e "\033[0;32m tuic://$UUID:password123@$IP:$PORT2?sni=www.bing.com&alpn=h3&congestion_control=bbr#${USERNAME} \033[0m"
 echo ""
