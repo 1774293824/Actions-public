f14f5d549cb6600339ee156789ff7eede3f4cfce
1774293824 <95674405+1774293824@users.noreply.github.com>
2024-10-17 17:49:44 +0800

    Create add-openclash-core.sh

---
+++ add-openclash-core.sh
@@ -0,0 +1,11 @@
+mkdir -p files/etc/openclash/core
+
+CLASH_META_URL="https://raw.githubusercontent.com/1774293824/Actions-public/main/clash.meta-linux-amd64-v1.16.0.gz"
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+
+wget -qO- $CLASH_META_URL | gunzip -c > files/etc/openclash/core/clash_meta
+wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+
+chmod +x files/etc/openclash/core/clash*
