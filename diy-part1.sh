734f70011879774bfa551092fb2f2b6ea3591835
1774293824 <95674405+1774293824@users.noreply.github.com>
2024-10-21 11:18:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,5 +12,5 @@
 # echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 
 # 小插件包
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
