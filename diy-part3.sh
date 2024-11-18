91336c7bdf56d48278f59e59684c85a6b5095ec3
1774293824 <95674405+1774293824@users.noreply.github.com>
2024-11-18 18:26:43 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -4,8 +4,7 @@
 # 京东云亚瑟固件架构（linux） = arm64
 
 mkdir -p files/etc/openclash/core
-
-CLASH_META_URL="https://raw.githubusercontent.com/1774293824/Actions-public/main/clash.meta-linux-arm64-v1.16.0.gz"
+CLASH_META_URL="https://raw.githubusercontent.com/1774293824/Github_files/blob/main/clash.meta-linux-arm64-v1.16.0.gz"
 # GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 # GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 
