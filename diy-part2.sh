f45c0f43e575b9ddff33c581b1dcb12d32313367
1774293824 <95674405+1774293824@users.noreply.github.com>
2024-12-07 20:12:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,8 +26,8 @@ sed -i '/set wireless.radio${devidx}.disabled=0/a\\t\t\tset wireless.radio${devi
 
 # 设置5Gwifi名为Owrt，设置LED为蓝色关闭，绿色开启
 sed -i "/uci commit luci/a uci set wireless.default_radio0.ssid='OWrt'\nuci commit wireless\nwifi reload" package/lean/default-settings/files/zzz-default-settings
-sed -i "/delete system.ntp.server/i\\\tset system.cfg048bba=led\n\\tset system.cfg048bba.default=0\n\\tset system.cfg048bba.name=Blue\n\\tset system.cfg048bba.sysfs=blue:status\n\\tset system.cfg048bba.trigger=none" package/lean/default-settings/files/zzz-default-settings
-sed -i "/delete system.ntp.server/i\\\tset system.cfg038bba=led\n\\tset system.cfg038bba.default=1\n\\tset system.cfg038bba.name=Green\n\\tset system.cfg038bba.sysfs=green:status\n\\tset system.cfg038bba.trigger=none" package/lean/default-settings/files/zzz-default-settings
+# sed -i "/delete system.ntp.server/i\\\tset system.cfg048bba=led\n\\tset system.cfg048bba.default=0\n\\tset system.cfg048bba.name=Blue\n\\tset system.cfg048bba.sysfs=blue:status\n\\tset system.cfg048bba.trigger=none" package/lean/default-settings/files/zzz-default-settings
+# sed -i "/delete system.ntp.server/i\\\tset system.cfg038bba=led\n\\tset system.cfg038bba.default=1\n\\tset system.cfg038bba.name=Green\n\\tset system.cfg038bba.sysfs=green:status\n\\tset system.cfg038bba.trigger=none" package/lean/default-settings/files/zzz-default-settings
 
 
 # 设置wan口上网方式为PPPOE，本地编译时在文件的第86行左右
