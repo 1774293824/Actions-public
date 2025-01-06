db5e124850a2119630e2c8e52a05ea31b05db793
1774293824 <95674405+1774293824@users.noreply.github.com>
2025-01-06 16:57:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,5 @@
 
 # 小插件包
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # echo 'src-git pkg https://github.com/1774293824/openwrt-pkg' >>feeds.conf.default
