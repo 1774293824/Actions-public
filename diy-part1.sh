c38cfad3543ff51f9268b2fed9287cdee1c14882
1774293824 <95674405+1774293824@users.noreply.github.com>
2025-03-26 17:03:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,8 @@
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-# 这个passwall经测试，用不了
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# 大插件包
-# echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
-
-# 小插件包
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# 这个仓库的代码代码更新比较频繁
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-# echo 'src-git pkg https://github.com/1774293824/openwrt-pkg' >>feeds.conf.default
+
+# 这个仓库的代码是不怎么更新的
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+
+# 大插件包,包含了上面的“kenzo”和“small”
+# echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
