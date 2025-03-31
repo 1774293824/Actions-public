67f9f32529af4dbc28bbe38a1d00ba7341b0e81d
1774293824 <95674405+1774293824@users.noreply.github.com>
2025-03-31 17:03:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,5 +7,4 @@
 # 大插件包,包含了上面的“kenzo”和“small”
 # echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 
-echo 'src-git small https://github.com/1774293824/small' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/1774293824/openwrt-packages' >>feeds.conf.default
