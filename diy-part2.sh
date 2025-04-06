91f31bd58b99eaa1dc820a516c3eba8e974add63
1774293824 <95674405+1774293824@users.noreply.github.com>
2025-04-06 21:09:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,24 +2,24 @@
 # # sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci/Makefile
 
 # # Modify default IP（修改wan口IP）本地编译时在文件的第150行左右
-# sed -i 's/192.168.1.1/192.168.68.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.68.1/g' package/base-files/files/bin/config_generate
 
 # # 新版LUCI的ip修改地址
-# sed -i 's/192.168.1.1/192.168.68.1/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.68.1/g' package/base-files/luci2/bin/config_generate
 
 # # 更新为root用户的默认密码为【admin】
-# sed -i 's|root:::0:99999:7:::|root:$1$ZrdSUj0o$MjkG92YkikpJJ4LHXI8uT1:20013:0:99999:7:::|g' package/base-files/files/etc/shadow
+sed -i 's|root:::0:99999:7:::|root:$1$ZrdSUj0o$MjkG92YkikpJJ4LHXI8uT1:20013:0:99999:7:::|g' package/base-files/files/etc/shadow
 # # 可选密码
 # # root:$1$ZrdSUj0o$MjkG92YkikpJJ4LHXI8uT1:20013:0:99999:7:::【admin】
 # # root:$1$d.4wFNEh$eGr/CydIez04zsnfDdqPa0:20013:0:99999:7:::【root】
 
 # # Modify hostname（设置设备名称）本地编译时在文件的第298行左右
-# sed -i 's/OpenWrt/OWrt/g' package/base-files/files/bin/config_generate
-# sed -i 's/LEDE/OWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/OWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/OWrt/g' package/base-files/files/bin/config_generate
 
 # # 设置wifi加密方式为psk2+ccmp,wifi密码为88888889
-# sed -i 's/encryption=none/encryption=psk2+ccmp/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-# sed -i '/set wireless.default_radio${devidx}.encryption=psk2+ccmp/a\\t\t\tset wireless.default_radio${devidx}.key=88888889' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/encryption=none/encryption=psk2+ccmp/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/set wireless.default_radio${devidx}.encryption=psk2+ccmp/a\\t\t\tset wireless.default_radio${devidx}.key=88888889' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # # 设置无线的国家代码为CN,wifi的默认功率为20
 # # sed -i 's/country=US/country=CN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
