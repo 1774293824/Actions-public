b259f701e19c310bafec6bcd793468dd8b2281c3
1774293824 <95674405+1774293824@users.noreply.github.com>
2025-04-11 14:12:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,12 +1,12 @@
 # 这个仓库的代码代码更新比较频繁
-# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 # 这个仓库的代码是不怎么更新的
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 
 # 大插件包,包含了上面的“kenzo”和“small”
 # echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 
-echo 'src-git kenzo https://github.com/1774293824/openwrt-packages' >>feeds.conf.default
+# echo 'src-git kenzo https://github.com/1774293824/openwrt-packages' >>feeds.conf.default
 
-echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+# echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
